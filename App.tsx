c73ac9abc477ea48e6bf0125320c14ab6f6389ff
John Hamm <johndhamm1@gmail.com>
2020-06-04 17:55:44 -0500

    add styled-components

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StyledText } from './App.styles';
 
 export default function App(): JSX.Element {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <StyledText>Did? Do. Done!</StyledText>
     </View>
   );
 }
 
+const BADASS = '#bada55';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#bada55',
+    backgroundColor: BADASS,
     alignItems: 'center',
     justifyContent: 'center',
   },
