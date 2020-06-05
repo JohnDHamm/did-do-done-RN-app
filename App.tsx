5173079ed484cabaad37f9f6c98cb88160490fd8
John Hamm <johndhamm1@gmail.com>
2020-06-05 13:24:07 -0500

    refactor(screens): move to src dir

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { EventScreen, RecurringScreen, SearchScreen } from './screens';
+import { EventScreen, RecurringScreen, SearchScreen } from './src/screens';
 
 declare global {
   type RootStackParamList = {
