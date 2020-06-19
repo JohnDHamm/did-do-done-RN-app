b6b10b81ad4a15e599b6038fcd952b6dcdaa4d95
John Hamm <johndhamm1@gmail.com>
2020-06-19 16:09:17 -0500

    feat(functions): add getRecurFreqData function

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,5 +1,11 @@
 import composeRecurData from './composeRecurData/composeRecurData';
 import getRecurDateString from './getRecurDateString/getRecurDateString';
+import getRecurFreqData from './getRecurFreqData/getRecurFreqData';
 import getRecurTotals from './getRecurTotals/getRecurTotals';
 
-export { composeRecurData, getRecurDateString, getRecurTotals };
+export {
+  composeRecurData,
+  getRecurDateString,
+  getRecurFreqData,
+  getRecurTotals,
+};
