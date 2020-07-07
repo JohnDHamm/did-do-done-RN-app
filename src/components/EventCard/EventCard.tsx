a7cedfbe9300a4fe6b46751bc0d94cb52eabd44d
John Hamm <johndhamm1@gmail.com>
2020-07-07 11:14:20 -0500

    feat(functions): add formatEventCardRecurInfo

--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -15,6 +15,7 @@ import Tag from '../Tag/Tag';
 import IMAGES from '../../../assets/images';
 import moment from 'moment';
 import { TagsContext } from '../../contexts';
+import { formatEventCardRecurInfo } from '../../functions';
 
 const EventCard: React.FC<SavedEvent> = ({
   name,
@@ -29,22 +30,6 @@ const EventCard: React.FC<SavedEvent> = ({
   if (savedYear !== moment().year()) {
     displayDate = displayDate + `, '${savedYear.toString().slice(2, 4)}`;
   }
-
-  const getRecurDate = (): string => {
-    const nextDate = moment(recurs?.nextdate).format('MMM D');
-    let recurFreq = '(every ';
-
-    if (recurs?.days) {
-      recurFreq = recurFreq + (recurs?.days).toString() + ' days)';
-    }
-    if (recurs?.weeks) {
-      recurFreq = recurFreq + (recurs?.weeks).toString() + ' weeks)';
-    }
-    if (recurs?.months) {
-      recurFreq = recurFreq + (recurs?.months).toString() + ' months)';
-    }
-    return `${nextDate} ${recurFreq}`;
-  };
 
   const createTags = (tagIds: number[]) => {
     return tags.filter((tag) => tagIds.includes(tag.id));
@@ -76,7 +61,7 @@ const EventCard: React.FC<SavedEvent> = ({
         <RecurRow>
           <Icon source={IMAGES.RECUR_ICON} />
           <RecurMsg>do again:</RecurMsg>
-          <RecurDate>{getRecurDate()}</RecurDate>
+          <RecurDate>{formatEventCardRecurInfo(recurs)}</RecurDate>
         </RecurRow>
       )}
       {notes && <Notes>{notes}</Notes>}
