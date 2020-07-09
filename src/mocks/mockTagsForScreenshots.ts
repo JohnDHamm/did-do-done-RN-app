b7fa7132707d9889f996f2ce921a309eab62634a
John Hamm <johndhamm1@gmail.com>
2020-07-09 18:32:05 -0500

    feat(mocks): add mocks for screenshots

---
+++ src/mocks/mockTagsForScreenshots.ts
@@ -0,0 +1,19 @@
+import { COLORS } from '../styles';
+
+export const mockTagsForScreenshots: Array<Tag> = [
+  {
+    id: 1,
+    name: 'car',
+    color: COLORS.TAG_COLORS[0].color,
+  },
+  {
+    id: 2,
+    name: 'home',
+    color: COLORS.TAG_COLORS[1].color,
+  },
+  {
+    id: 3,
+    name: 'safety',
+    color: COLORS.TAG_COLORS[8].color,
+  },
+];
